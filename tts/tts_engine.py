453dfe7d795f525af7f3c4cb2ea4d95fb6a88071
yuktha-c <yuktha.c04@gmail.com>
2025-06-14 09:29:30 +0530

    Basic code for tts added

---
+++ tts/tts_engine.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+
+text = "Doing a basic text run to see if tts is working"
+tts = gTTS(text)
+fp = io.BytesIO()
+tts.write_to_fp(fp)
+fp.seek(0)
+
+audio = AudioSegment.from_file(fp, format="mp3")
+play(audio)
